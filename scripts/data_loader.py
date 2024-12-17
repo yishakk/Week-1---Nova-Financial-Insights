97556a3331d6416db5acbd7056f25bf79a2b36fd
Yishak Kibru <70431682+yishakk@users.noreply.github.com>
2024-12-17 21:59:27 +0300

    align stock data with news data

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -3,6 +3,9 @@ import pandas as pd
 def load_data(file_path):
     return pd.read_csv(file_path)
 
+def load_data_with_parse(file_path, parse_dates=None):
+    return pd.read_csv(file_path, parse_dates=None)
+
 def load_csv_files(file_paths, stock_names):
     dataframes = []
     for file_path, stock in zip(file_paths, stock_names):
